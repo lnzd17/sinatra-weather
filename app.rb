f66bfde90994788985307fb83ad9b8f3dde062e2
lnzd17 <lindsaykayedutton@gmail.com>
2017-09-10 10:52:33 -0400

    connected to weather api

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
+require 'config_env'
+require 'httparty'
+ConfigEnv.init("#{__dir__}/config/env.rb")
 
 set :port, 3000
 
 get '/' do
+  @api_key = ENV['API_KEY']
+  res = HTTParty.get(
+    "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=#{ENV['API_KEY']}"
+  )
+  @data = JSON.parse(res.body)
+  @temp = (@data['main']['temp'] * 9 / 5 - 459.67).floor
+
   erb :index
 end
